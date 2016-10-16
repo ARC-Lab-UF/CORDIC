78b9048bd04b65f585d318900cc92d9087b4874c
Austin Baylis <abaylis@ufl.edu>
2016-10-16 10:24:45 -0400

    better pipeline imp

--- src/pipeline.vhd
+++ src/pipeline.vhd
@@ -29,119 +29,118 @@ architecture STR of pipeline is
 	signal Xnext,Ynext,thetanext : array_type1;
 	signal modeBuff : array_type2;
 begin	
+
+	XR0: entity work.gen_reg
+		GENERIC MAP
+		(
+			WIDTH => WIDTH
+		)
+		PORT MAP
+		(
+			clk 		=> clk,
+			rst		=> rst,
+			input		=> Xin,
+			output	=> X(0)
+		);
+		
+	YR0: entity work.gen_reg
+		GENERIC MAP
+		(
+			WIDTH => WIDTH
+		)
+		PORT MAP
+		(
+			clk 		=> clk,
+			rst		=> rst,
+			input		=> Yin,
+			output	=> Y(0)
+		);
+		
+	TR0: entity work.gen_reg
+		GENERIC MAP
+		(
+			WIDTH => WIDTH
+		)
+		PORT MAP
+		(
+			clk 		=> clk,
+			rst		=> rst,
+			input		=> thetain,
+			output	=> theta(0)
+		);
+
+	MR0: entity work.gen_reg
+		GENERIC MAP
+		(
+			WIDTH => 1
+		)
+		PORT MAP
+		(
+			clk 		=> clk,
+			rst		=> rst,
+			input		=> (0 => mode),
+			output	=> modeBuff(0)
+		);
+				
 	G1: for I in 0 to ROUNDS-1 generate
-		-- Generate CORE for the first round
+		CORE_I: entity work.cordic_core
+			GENERIC MAP
+			(
+				WIDTH => WIDTH
+			)
+			PORT MAP
+			(
+				mode		=> modeBuff(I)(0),
+				Xin		=>	X(I),
+				Yin		=> Y(I),
+				thetain	=> theta(I),
+				delta		=> (others => '0'),
+				Xout		=> Xnext(I),
+				Yout		=> Ynext(I),
+				thetaout => thetanext(I)
+			);
+			
+		XRI: entity work.gen_reg
+			GENERIC MAP
+			(
+				WIDTH => WIDTH
+			)
+			PORT MAP
+			(
+				clk 		=> clk,
+				rst		=> rst,
+				input		=> Xnext(I),
+				output	=> X(I+1)
+			);
+			
+		YRI: entity work.gen_reg
+			GENERIC MAP
+			(
+				WIDTH => WIDTH
+			)
+			PORT MAP
+			(
+				clk 		=> clk,
+				rst		=> rst,
+				input		=> Ynext(I),
+				output	=> Y(I+1)
+			);
+			
+		TRI: entity work.gen_reg
+			GENERIC MAP
+			(
+				WIDTH => WIDTH
+			)
+			PORT MAP
+			(
+				clk 		=> clk,
+				rst		=> rst,
+				input		=> thetanext(I),
+				output	=> theta(I+1)
+			);
 		
-		G2: if (I = 0) generate
-			XR0: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> Xin,
-					output	=> X(I)
-				);
-				
-			YR0: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> Yin,
-					output	=> Y(I)
-				);
-				
-			TR0: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> thetain,
-					output	=> theta(I)
-				);
-		
-			MR0: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => 1
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> (0 => mode),
-					output	=> modeBuff(I)
-				);
-				
-			CORE_1: entity work.cordic_core
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					mode		=> modeBuff(I)(0),
-					Xin		=>	X(I),
-					Yin		=> Y(I),
-					thetain	=> theta(I),
-					delta		=> (others => '0'),
-					Xout		=> Xnext(I),
-					Yout		=> Ynext(I),
-					thetaout => thetanext(I)
-				);
-				
-			XR1: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> Xnext(I),
-					output	=> X(I+1)
-				);
-				
-			YR1: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> Ynext(I),
-					output	=> Y(I+1)
-				);
-				
-			TR1: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> thetanext(I),
-					output	=> theta(I+1)
-				);
-				
-			MR1: entity work.gen_reg
+		G2: if I /= ROUNDS-1 generate
+			MRI: entity work.gen_reg
 				GENERIC MAP
 				(
 					WIDTH => 1
@@ -154,80 +153,6 @@ begin
 					output	=> modeBuff(I+1)
 				);
 		end generate G2;
-		
-		-- Generate CORE for all the rounds in between
-		G3: if (I /= 0) generate
-			CORE_I: entity work.cordic_core
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					mode		=> modeBuff(I)(0),
-					Xin		=>	X(I),
-					Yin		=> Y(I),
-					thetain	=> theta(I),
-					delta		=> (others => '0'),
-					Xout		=> Xnext(I),
-					Yout		=> Ynext(I),
-					thetaout => thetanext(I)
-				);
-				
-			XRI: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> Xnext(I),
-					output	=> X(I+1)
-				);
-				
-			YRI: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> Ynext(I),
-					output	=> Y(I+1)
-				);
-				
-			TRI: entity work.gen_reg
-				GENERIC MAP
-				(
-					WIDTH => WIDTH
-				)
-				PORT MAP
-				(
-					clk 		=> clk,
-					rst		=> rst,
-					input		=> thetanext(I),
-					output	=> theta(I+1)
-				);
-			
-			G4: if I /= ROUNDS-1 generate
-				MRI: entity work.gen_reg
-					GENERIC MAP
-					(
-						WIDTH => 1
-					)
-					PORT MAP
-					(
-						clk 		=> clk,
-						rst		=> rst,
-						input		=> modeBuff(I),
-						output	=> modeBuff(I+1)
-					);
-				end generate G4;
-			end generate G3;
 	end generate G1;
 	
 	Xout <= X(ROUNDS);
