8a313851133d380899e911ff911fd5c4cc8e99b9
Austin Baylis <abaylis@ufl.edu>
2016-11-27 13:47:44 -0500

    cordic_core_tb finished. Everything tests ok after 625,000,000 iterations

--- tb/cordic_core_tb.vhd
+++ tb/cordic_core_tb.vhd
@@ -73,91 +73,192 @@ begin
 		variable shifted_X,
 					shifted_Y	: integer;
 		variable Xout_ex,
-					Yout_ex		: std_logic_vector(WIDTH-1 downto 0) := (others => '0');
+					Yout_ex,
+					theta_ex		: std_logic_vector(WIDTH-1 downto 0) := (others => '0');
 	begin
 	
-		wait for 200 ns; -- Just a pause at the start
+		wait for 10 ns; -- Just a pause at the start
 		
 		itr_loop : for I in 0 to 50 loop
 			x_loop : for X in 0 to 50 loop
 				y_loop : for Y in -50 to 50 loop
-					-- Set the values to input into CORDIC core
-					itr <= i;
-					Xin <= std_logic_vector(to_signed(X, WIDTH));
-					Yin <= std_logic_vector(to_signed(Y, WIDTH));
-			
-					mode <= '0';
-					wait for 10 ns; -- wait for output
+					theta_loop : for THETA in 0 to 50 loop
+						delta_loop : for DELTAA in 0 to 50 loop
+							-- Set the values to input into CORDIC core
+							itr 		<= i;
+							Xin 		<= std_logic_vector(to_signed(X, WIDTH));
+							Yin 		<= std_logic_vector(to_signed(Y, WIDTH));
+							thetain 	<= std_logic_vector(to_signed(THETA, WIDTH));
+							delta		<= std_logic_vector(to_signed(DELTAA, WIDTH));
 					
-					-- Check the outputs
-					shift_value := 2**REAL(-I);
-					
-					shifted_X	:= integer(floor(real(to_integer(signed(Xin))) * shift_value));
-					shifted_Y 	:= integer(floor(real(to_integer(signed(Yin))) * shift_value));
-					
-					
-					if (signed(Yin) >= 0) then
-						Xout_ex 		:= std_logic_vector(signed(Xin) + to_signed(shifted_Y, WIDTH));
-						Yout_ex 		:= std_logic_vector(signed(Yin) - to_signed(shifted_X, WIDTH));
-						
-						assert (Xout = Xout_ex)
-							report 	"!!! Xout was incorrect !!!" & CR &
-										"Mode = 0" 		& CR &
-										"Y Positive"	& CR &
-										"ITR = " 		& integer'image(I) & CR &
-										"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
-										"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
-										CR & -- Add a little space
-										"Xout = "		& integer'image(to_integer(signed(Xout))) & CR &
-										"Xout_ex = " 	& integer'image(to_integer(signed(Xout_ex)));
-										
-						assert (Yout = Yout_ex)
-							report 	"!!! Yout was incorrect !!!" & CR &
-										"Mode = 0" 		& CR &
-										"Y Positive"	& CR &
-										"ITR = " 		& integer'image(I) & CR &
-										"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
-										"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
-										CR & -- Add a little space
-										"Yout = "		& integer'image(to_integer(signed(Yout))) & CR &
-										"Yout_ex = " 	& integer'image(to_integer(signed(Yout_ex)));
-					else
-						Xout_ex 		:= std_logic_vector(signed(Xin) - to_signed(shifted_Y, WIDTH));
-						Yout_ex 		:= std_logic_vector(signed(Yin) + to_signed(shifted_X, WIDTH));
-						
-						assert (Xout = Xout_ex)
-							report 	"!!! Xout was incorrect !!!" & CR &
-										"Mode = 0" 		& CR &
-										"Y Negative"	& CR &
-										"ITR = " 		& integer'image(I) & CR &
-										"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
-										"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
-										CR & -- Add a little space
-										"Xout = "		& integer'image(to_integer(signed(Xout))) & CR &
-										"Xout_ex = " 	& integer'image(to_integer(signed(Xout_ex)));
-										
-						assert (Yout = Yout_ex)
-							report 	"!!! Yout was incorrect !!!" & CR &
-										"Mode = 0" 		& CR &
-										"Y Negative"	& CR &
-										"ITR = " 		& integer'image(I) & CR &
-										"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
-										"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
-										CR & -- Add a little space
-										"Yout = "		& integer'image(to_integer(signed(Yout))) & CR &
-										"Yout_ex = " 	& integer'image(to_integer(signed(Yout_ex)));
-					end if;
-					
-					wait for 10 ns; -- wait a little longer
+							mode <= '0';
+							wait for 1 ns; -- wait for output
+							
+							-- Check the outputs
+							shift_value := 2**REAL(-I);
+							
+							shifted_X	:= integer(floor(real(to_integer(signed(Xin))) * shift_value));
+							shifted_Y 	:= integer(floor(real(to_integer(signed(Yin))) * shift_value));
+							
+							
+							if (signed(Yin) >= 0) then
+								Xout_ex 		:= std_logic_vector(signed(Xin) + to_signed(shifted_Y, WIDTH));
+								Yout_ex 		:= std_logic_vector(signed(Yin) - to_signed(shifted_X, WIDTH));
+								theta_ex		:= std_logic_vector(signed(thetain) + signed(delta));
+								
+								assert (Xout = Xout_ex)
+									report 	"!!! Xout was incorrect !!!" & CR &
+												"Mode = 0" 		& CR &
+												"Y Positive"	& CR &
+												"ITR = " 		& integer'image(I) & CR &
+												"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
+												"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
+												CR & -- Add a little space
+												"Xout = "		& integer'image(to_integer(signed(Xout))) & CR &
+												"Xout_ex = " 	& integer'image(to_integer(signed(Xout_ex)));
+												
+								assert (Yout = Yout_ex)
+									report 	"!!! Yout was incorrect !!!" & CR &
+												"Mode = 0" 		& CR &
+												"Y Positive"	& CR &
+												"ITR = " 		& integer'image(I) & CR &
+												"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
+												"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
+												CR & -- Add a little space
+												"Yout = "		& integer'image(to_integer(signed(Yout))) & CR &
+												"Yout_ex = " 	& integer'image(to_integer(signed(Yout_ex)));
+												
+								assert (thetaout = theta_ex)
+									report 	"!!! thetaout was incorrect !!!" & CR &
+												"Mode = 0" 		& CR &
+												"Y Positive"	& CR &
+												"thetain = " 	& integer'image(to_integer(signed(thetain))) & CR &
+												"delta = " 		& integer'image(to_integer(signed(delta))) & CR &
+												CR & -- Add a little space
+												"thetaout = "	& integer'image(to_integer(signed(thetaout))) & CR &
+												"theta_ex = " 	& integer'image(to_integer(signed(theta_ex)));
+							else
+								Xout_ex 		:= std_logic_vector(signed(Xin) - to_signed(shifted_Y, WIDTH));
+								Yout_ex 		:= std_logic_vector(signed(Yin) + to_signed(shifted_X, WIDTH));
+								theta_ex		:= std_logic_vector(signed(thetain) - signed(delta));
+								
+								assert (Xout = Xout_ex)
+									report 	"!!! Xout was incorrect !!!" & CR &
+												"Mode = 0" 		& CR &
+												"Y Negative"	& CR &
+												"ITR = " 		& integer'image(I) & CR &
+												"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
+												"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
+												CR & -- Add a little space
+												"Xout = "		& integer'image(to_integer(signed(Xout))) & CR &
+												"Xout_ex = " 	& integer'image(to_integer(signed(Xout_ex)));
+												
+								assert (Yout = Yout_ex)
+									report 	"!!! Yout was incorrect !!!" & CR &
+												"Mode = 0" 		& CR &
+												"Y Negative"	& CR &
+												"ITR = " 		& integer'image(I) & CR &
+												"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
+												"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
+												CR & -- Add a little space
+												"Yout = "		& integer'image(to_integer(signed(Yout))) & CR &
+												"Yout_ex = " 	& integer'image(to_integer(signed(Yout_ex)));
+												
+								assert (thetaout = theta_ex)
+									report 	"!!! thetaout was incorrect !!!" & CR &
+												"Mode = 0" 		& CR &
+												"Y Negative"	& CR &
+												"thetain = " 	& integer'image(to_integer(signed(thetain))) & CR &
+												"delta = " 		& integer'image(to_integer(signed(delta))) & CR &
+												CR & -- Add a little space
+												"thetaout = "	& integer'image(to_integer(signed(thetaout))) & CR &
+												"theta_ex = " 	& integer'image(to_integer(signed(theta_ex)));
+							end if;
+							
+							wait for 1 ns; -- wait a little longer
+							
+							-- Change modes
+							mode <= '1';
+							wait for 1 ns;
+							
+							if (signed(thetain) >= 0) then
+								Xout_ex 		:= std_logic_vector(signed(Xin) - to_signed(shifted_Y, WIDTH));
+								Yout_ex 		:= std_logic_vector(signed(Yin) + to_signed(shifted_X, WIDTH));
+								theta_ex		:= std_logic_vector(signed(thetain) - signed(delta));
+								
+								assert (Xout = Xout_ex)
+									report 	"!!! Xout was incorrect !!!" & CR &
+												"Mode = 1" 		& CR &
+												"Theta Pos." 	& CR &
+												"ITR = " 		& integer'image(I) & CR &
+												"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
+												"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
+												CR & -- Add a little space
+												"Xout = "		& integer'image(to_integer(signed(Xout))) & CR &
+												"Xout_ex = " 	& integer'image(to_integer(signed(Xout_ex)));
+												
+								assert (Yout = Yout_ex)
+									report 	"!!! Yout was incorrect !!!" & CR &
+												"Mode = 1" 		& CR &
+												"Theta Pos." 	& CR &
+												"ITR = " 		& integer'image(I) & CR &
+												"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
+												"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
+												CR & -- Add a little space
+												"Yout = "		& integer'image(to_integer(signed(Yout))) & CR &
+												"Yout_ex = " 	& integer'image(to_integer(signed(Yout_ex)));
+												
+								assert (thetaout = theta_ex)
+									report 	"!!! thetaout was incorrect !!!" & CR &
+												"Mode = 1" 		& CR &
+												"Theta Pos."	& CR &
+												"thetain = " 	& integer'image(to_integer(signed(thetain))) & CR &
+												"delta = " 		& integer'image(to_integer(signed(delta))) & CR &
+												CR & -- Add a little space
+												"thetaout = "	& integer'image(to_integer(signed(thetaout))) & CR &
+												"theta_ex = " 	& integer'image(to_integer(signed(theta_ex)));
+							else
+								Xout_ex 		:= std_logic_vector(signed(Xin) + to_signed(shifted_Y, WIDTH));
+								Yout_ex 		:= std_logic_vector(signed(Yin) - to_signed(shifted_X, WIDTH));
+								theta_ex		:= std_logic_vector(signed(thetain) + signed(delta));
+								
+								assert (Xout = Xout_ex)
+									report 	"!!! Xout was incorrect !!!" & CR &
+												"Mode = 0" 		& CR &
+												"Theta Neg." 	& CR &
+												"ITR = " 		& integer'image(I) & CR &
+												"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
+												"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
+												CR & -- Add a little space
+												"Xout = "		& integer'image(to_integer(signed(Xout))) & CR &
+												"Xout_ex = " 	& integer'image(to_integer(signed(Xout_ex)));
+												
+								assert (Yout = Yout_ex)
+									report 	"!!! Yout was incorrect !!!" & CR &
+												"Mode = 0" 		& CR &
+												"Theta Neg." 	& CR &
+												"ITR = " 		& integer'image(I) & CR &
+												"Xin = " 		& integer'image(to_integer(signed(Xin))) & CR &
+												"Yin = " 		& integer'image(to_integer(signed(Yin))) & CR &
+												CR & -- Add a little space
+												"Yout = "		& integer'image(to_integer(signed(Yout))) & CR &
+												"Yout_ex = " 	& integer'image(to_integer(signed(Yout_ex)));
+												
+								assert (thetaout = theta_ex)
+									report 	"!!! thetaout was incorrect !!!" & CR &
+												"Mode = 1" 		& CR &
+												"Theta Neg."	& CR &
+												"thetain = " 	& integer'image(to_integer(signed(thetain))) & CR &
+												"delta = " 		& integer'image(to_integer(signed(delta))) & CR &
+												CR & -- Add a little space
+												"thetaout = "	& integer'image(to_integer(signed(thetaout))) & CR &
+												"theta_ex = " 	& integer'image(to_integer(signed(theta_ex)));
+							end if;
+						end loop;
+					end loop;
 				end loop;
 			end loop;
 		end loop;
-		
-		thetain <= std_logic_vector(to_signed(6717, WIDTH));
-		delta <= std_logic_vector(to_signed(gen_theta_value(7), WIDTH));
-		
-	
-		wait for 100 ns;
 		
 		wait; -- inf loop
 	end process;
